bc384da464a8646d99a26134426145cc62481c0d
IstvanBanszki <banszki.istvan@outlook.com>
2017-09-24 00:08:45 +0200

    Fixing parser errors

--- src/main/java/com/gdp/parser/JsonParser.java
+++ src/main/java/com/gdp/parser/JsonParser.java
@@ -2,10 +2,8 @@ package com.gdp.parser;
 
 import com.gdp.model.Country;
 import com.gdp.model.Organization;
-import static com.gdp.parser.AbstractParser.LOG;
 import java.util.LinkedList;
 import java.util.List;
-import static java.util.logging.Level.INFO;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,17 +18,17 @@ public class JsonParser extends AbstractParser implements IParser {
         String orgCountries = getOrganizationCountries(content, org);
         String[] countries = orgCountries.split(REGEX);
         
-        for (int index = 1; index <= countries.length; index ++) {
-            LOG.log(INFO, "Line {0}", countries[index]);
-            countryList.add(getCountry(countries[index]));
+        for (int index = 1; index < countries.length; index ++) {
+            if(countries[index].length() > 16) {
+                countryList.add(getCountry(countries[index]));
+            }
         }
         return countryList;
     }
     
     private Country getCountry(String line) {
-        String country = line.substring(line.lastIndexOf("flag\\|"), line.lastIndexOf("\\|\\|")-2);
-        int gdp = Integer.valueOf(line.substring(line.lastIndexOf("\\|\\|")+2).replaceAll(" ", ""));
-        System.out.println(country + gdp);
+        String country = line.substring(line.lastIndexOf("flag|")+5, line.lastIndexOf("}}"));
+        int gdp = Integer.valueOf(line.substring(line.lastIndexOf("||")+2).replaceAll(",", "").replaceAll(" ", ""));
         return new Country(country, gdp);
     }
     
